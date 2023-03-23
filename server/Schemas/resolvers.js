f947293cf0800dfd96954030cb16093792f43b7c
DylanF <dylanbrad10@gmail.com>
2023-03-23 15:52:10 -0400

    resolvers created

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("thoughts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { bookInput }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You need to be logged in!");
+      } else {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id}, 
+          {
+            $addToSet: {
+              savedBooks: bookInput, 
+            }
+          }, 
+          { new: true }
+        );
+
+        return user;
+      }
+    },
+    removeBook: async (parent, { bookInput }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError("You need to be logged in!");
+      } else {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id}, 
+          {
+            $pull: { savedBooks: bookInput }
+          }, 
+          { new: true }
+        );
+
+        return user;
+      }
+    }
+  },
+};
+
+module.exports = resolvers;
